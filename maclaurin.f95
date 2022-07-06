15ca713332323b8db48a37f1acd9e678e801b472
Gabriel Souza <101633298+Gabriel-Tapes@users.noreply.github.com>
2022-07-06 11:56:26 -0300

    Atualiza exercicio de serie de maclaurin para seno

--- maclaurin.f95
+++ maclaurin.f95
@@ -1,24 +1,34 @@
 program maclaurin
 implicit none
 
-integer :: M, n
-real :: x, aux, seno=0
+integer :: M=10, n
+real :: x, aux, seno=0, eps
    
    print *, 'Digite o valor de x: '; read *, x
-   print *, 'Digite o valor de M: '; read *, M
+   print *, 'Digite o valor de Epson: '; read *, eps
 
-   do n=0, M
-      aux = ((-1)**n) * ((x**(n+n+1)) / FAT(n+n+1))
-      seno = seno + aux
+   aux = abs(((-1)**M) * ((x**(M+M+1)) / FAT(M+M+1)))
+   
+   do while (aux >= eps .AND. M < 17)
+      M = M + 1
+
+      aux = abs(((-1)**M) * ((x**(M+M+1)) / FAT(M+M+1)))
+      print *, 'aux =', aux
+      print *, 'M =', M
    end do
 
-   print *, seno
+   print *, 'M =', M
+
+   do n=0, M
+      seno = seno + ((-1)**n) * ((x**(n+n+1)) / FAT(n+n+1))
+   end do
+
+   print *, 'Seno:', seno
+   print *, 'Sin:', sin(x)
 
 contains
    recursive integer(kind=8) function FAT (x) result (N)
       integer :: x
-      
-      if (x < 0) print *, 'Digite um valor maior que 0!'
 
       if (x == 0) then
          N = 1
