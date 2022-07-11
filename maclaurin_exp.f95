e5a8818bdeb92939ce98dddb96bf7c4e57053c17
Gabriel Souza <gabrielsouza.iacs@gmail.com>
2022-07-11 19:16:01 +0000

    Correção maclaurin_exp.f95

--- maclaurin_exp.f95
+++ maclaurin_exp.f95
@@ -1,60 +1,67 @@
 program maclaurin_exp
 implicit none
 
-integer :: z, M=0, k
-real :: ex, x, f, i, eps=1e-16, aux
+integer :: z, k
+real :: ex, x, f, eps=1e-14, aux, s
 
    print *, 'Digite o valor de x:'; read *, x
    
    z = x
    f = x - z
    
-   aux = 1
-
-   do while (aux > eps)
-      aux = (f ** M) / (fat(M))
-      M = M + 1
-   end do
-   print *, 'M:', M
+   k = 0
+   s = 0.0
    
-   aux = 0
-   do k=0, M
-      aux = aux + ((f**k) / fat(k))
-   end do
+   if (f /= 0.0) then
+     do 
+     	aux = f ** k / fat(k)
 
-   ex = eZ(z) * aux
+     	if (abs(aux) < eps) then
+			exit
+     	end if
 
-   print *, 'ex =', ex
-   print *, 'exp=', exp(x)
+     	s = s + aux
+     	k = k + 1
+   	 end do
+
+   else
+     s = 1.0
+   end if
+
+   ex = eZ(z) * s
+
+   print '("ex =", 1F20.14)', ex
+   print '("exp=", 1F20.14)', exp(x)
    
    contains
-      recursive real function fat (n) result (f)
-         integer :: n
-          
-         if (n == 0) then
-            f = 1
-         else
-            f = n * fat(n-1)
-         end if
+      real function fat (n) result (f)
+         implicit none
+
+		 integer, intent(in) :: n
+         integer :: i
+         
+		 f = 1
+
+         do i=2, n
+           f = f*i
+         end do
 
       end function fat
 
       real function eZ (z) result (r)
          integer :: z, k
-         real :: e
+         real, parameter :: e = 2.7182818284590452353602874713526624977572470936999595749669676277
          
-         e = exp(1.)
          r = 1
+		
+		 if (z /= 0) then
+         	do k=1, abs(z)
+            	r = r * e
+         	end do
 
-         if (z > 0) then
-            do k=1, z
-              r = r * e
-            end do
-         else
-            do k=1, abs(z)
-               r = r * e
-            end do
-            r = 1 / r
+            	if (z <0) then
+            		r = 1 / r
+         		end if
          end if
          
       end function eZ
