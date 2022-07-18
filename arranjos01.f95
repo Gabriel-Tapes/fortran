61d03506c284db642d0d2b07ee5ba73774e53e2a
gabriel-tapes <gabrielsouza.iacs@gmail.com>
2022-07-18 12:58:55 -0300

    adiciona exercicios 1 e 2 de arranjos

---
+++ arranjos01.f95
@@ -0,0 +1,12 @@
+program arranjos01
+implicit none
+
+real, dimension(4) :: X = (/1.0, 3.0, 4.0, 10.0/)
+
+   X = X * 5
+
+   print '(4F8.2)', X
+
+   print '("Soma:", 1F8.2, /, "Produto:" 1F10.2)', sum(X), product(X)
+
+end program arranjos01
